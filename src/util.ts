a98645f5728351dd7b07e301c8fc97625e5e748f
Safin Singh <safin@safins-air.lan>
2020-08-21 16:11:38 -0700

    bugfixes and simplification

--- src/util.ts
+++ src/util.ts
@@ -6,55 +6,69 @@ interface validCheck {
   reaction?: Discord.EmojiResolvable
 }
 
-const willNotOverflow = (msg: Discord.Message): boolean => {
-  const arr = msg.content.split(' ')
-  const iter = parseInt(arr[1]) + 1
+const willOverflow = (msgArr: Array<string>): boolean => {
+  const iter = parseInt(msgArr[1]) + 1
 
-  if (iter * arr.slice(2).join(' ').length > 1000) {
-    return false
+  if (iter * (iter - 1) * msgArr.slice(2).join(' ').length > 1000) {
+    return true
   }
-  return true
+  return false
 }
 
 export const isValid = (msg: Discord.Message): validCheck => {
-  if (msg.content.length < 3) {
+  const msgArr = msg.content.split(' ')
+
+  if (msgArr.length < 3) {
+    console.log('1')
     return {
       isValid: false,
       error: 'Invalid command, must have at least 3 arguments!',
       reaction: '🗑️',
     }
+  } else if (isNaN(parseInt(msgArr[1]))) {
+    return {
+      isValid: false,
+      error: 'Invalid number, must be an integer!',
+      reaction: '🗑️',
+    }
   } else {
+    let toReturn: validCheck = {
+      isValid: true,
+    }
+
     msg.content
       .split(' ')
       .slice(1)
       .forEach((e) => {
-        const overflow = willNotOverflow(msg)
-        switch (e) {
-          case '/pyramid':
-            return {
-              isValid: true,
-              error: 'Recursiveness is not allowed!',
-              reaction: '😡',
-            }
-            break
-          case '͔':
-            return {
-              isValid: false,
-              error: "Sorry, but that character doesn't work :(",
-              reaction: '😔',
-            }
-          default:
-            return {
-              isValid: overflow,
-              error:
-                overflow &&
-                'Whoops! Looks like that exceeds the maximum characters!',
-              reaction: overflow && '😔',
-            }
+        if (e === '/pyramid') {
+          toReturn = {
+            isValid: false,
+            error: 'Recursiveness is not allowed!',
+            reaction: '😡',
+          }
+        } else if (e === '͔') {
+          toReturn = {
+            isValid: false,
+            error: "Sorry, but that character doesn't work :(",
+            reaction: '😔',
+          }
         }
       })
-  }
-  return {
-    isValid: false,
+
+    if (!toReturn.isValid) {
+      return toReturn
+    }
+
+    if (willOverflow(msgArr)) {
+      return {
+        isValid: false,
+        error: 'Whoops! Looks like that exceeds the maximum characters!',
+        reaction: '😔',
+      }
+    } else {
+      return {
+        isValid: true,
+      }
+    }
   }
 }
